56d05219a6657281a1b9a7d779ef4619f82c2d80
svulinovic <Suzana.Vulinovic@trilix.eu>
2020-07-16 21:54:58 +0200

    init

---
+++ src/test/java/com/svulinovic/greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svulinovic.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
